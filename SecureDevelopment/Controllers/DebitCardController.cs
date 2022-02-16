bd38abb0aebae0479f785aa822c5dda41277ee97
Uryupin <urdmitriy@yandex.ru>
2022-02-16 15:52:40 +0300

    create controller, test

---
+++ SecureDevelopment/Controllers/DebitCardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecureDevelopment.Controllers
+{
+    public class DebitCardController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
